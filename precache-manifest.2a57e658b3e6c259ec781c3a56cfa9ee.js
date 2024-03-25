e15464057ad000ba7b0fe718254ffb49be5ad870
izzie <izzienjeri12@gmail.com>
2024-03-25 04:14:08 +0300

    Updates

---
+++ precache-manifest.2a57e658b3e6c259ec781c3a56cfa9ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a414df3181abaf2d828ef1261d77c2b2",
+    "url": "/izzie-portfolio/index.html"
+  },
+  {
+    "revision": "a7f84d5f3d874f4758a1",
+    "url": "/izzie-portfolio/static/css/main.28cb31f8.chunk.css"
+  },
+  {
+    "revision": "0f2e6c5f89b0d0eae3ca",
+    "url": "/izzie-portfolio/static/js/2.0cb7b136.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/izzie-portfolio/static/js/2.0cb7b136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f84d5f3d874f4758a1",
+    "url": "/izzie-portfolio/static/js/main.4bb67cc4.chunk.js"
+  },
+  {
+    "revision": "1efdb797b11cefd29f59",
+    "url": "/izzie-portfolio/static/js/runtime-main.598c044c.js"
+  },
+  {
+    "revision": "149348ea954e15be83feff3635422a58",
+    "url": "/izzie-portfolio/static/media/coffee-apple.149348ea.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bad5df4866159828bd119b99c48ff3b1.js"
+  "/izzie-portfolio/precache-manifest.2a57e658b3e6c259ec781c3a56cfa9ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/izzie-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
